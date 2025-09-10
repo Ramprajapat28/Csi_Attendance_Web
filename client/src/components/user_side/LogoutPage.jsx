10709e5638c135b7b2d3f2edadcb09153f277caa
shubhammoreeee <shubhammore14112006@gmail.com>
2025-09-10 12:25:27 +0530

    logout page connected

--- client/src/components/user_side/LogoutPage.jsx
+++ client/src/components/user_side/LogoutPage.jsx
@@ -10,27 +10,29 @@ const Logout = () => {
         logout()
         navigate('/')
     }
-    // const cancel = () => navigate("/Dashboard");
+    const cancel = () => navigate("/Dashboard");
     return (
     <div>
-        <div className="teacher-info">
-            {/* <img
+        <div className='space1 flex justify-center items-center h-screen w-screen bg-gray-500'>
+        <div className="teacher-info h-[613px] w-[353px] bg-white flex flex-col justify-center items-center rounded-3xl">
+            <img
         onClick={cancel}
         src="/src/assets/cross.png"
-        className="h-[12px] absolute right-[15px] top-[25px] cursor-pointer"
+        className="h-[12px] cursor-pointer relative left-[150px]"
         alt="Cancel"
-        /> */}
-            <div className="avatar flex flex-col justify-center items-center h-[600px]">
+        />
+            <div className="avatar flex flex-col justify-center items-center h-[210px] w-[156px] m-[150px]">
                 <img src="/src/assets/Avatar.png" alt="avatar" className='h-[182px] w-[182px]'/>
                 <span className='font-medium text-[24px]'>{user?.name}</span>
                 <span className='text-gray-600 text-[16px]'>Teacher, EXTC dept.</span>
             </div>
             <div className="qr flex flex-col justify-center items-center gap-[8px]">
-                <button onClick={hidden} className='flex justify-center items-center rounded-lg text-sm font-normal gap-3 bg-[#1D61E7] text-white w-[350px] h-[48px] shadow-[0px_4px_4px_0px_#00000040] '> Log Out </button>
+                <button onClick={hidden} className='flex justify-center items-center rounded-lg text-sm font-normal gap-3 bg-[#1D61E7] text-white w-[300px] h-[48px] shadow-[0px_4px_4px_0px_#00000040] '> Log Out </button>
             </div>
+        </div>
         </div>
     </div>
     )
 }
-
+// absolute right-[15px] top-[25px]
 export default Logout
