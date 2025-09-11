19d841f0940dfb4a0bac7f2455e263b6fcc55d73
sahil-111-p <sahilpanchigar@gmail.com>
2025-09-11 16:26:01 +0530

    navbar done

--- client/src/components/Admin_side/Admin_Navbar.jsx
+++ client/src/components/Admin_side/Admin_Navbar.jsx
@@ -1,74 +1,133 @@
 import React from "react";
+import { Search } from "lucide-react";
 
 export const Admin_Navbar = () => {
   return (
     <div>
-      {/* <div className="navbar bg-base-100 shadow-sm "> */}
-        {/* <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
+       <div className="navbar bg-base-200 shadow-sm flex justify-around px-[104px] py-[24px]">
+        <a className="btn btn-ghost text-xl w-[231px] h-[44px]">
+          <img src="/src/assets/logo.svg" alt="" />
+        </a>
+        <input
+          type="text"
+          placeholder="Search   🔍︎"
+          className="input w-24 md:w-[60%] placeholder:text-center placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-black focus:border-transparent"
+        />
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost ">
+            <div className="flex gap-[5px]">
+              <img
+                className=" w-6 "
+                alt="Tailwind CSS Navbar component"
+                src="/src/assets/Profile.svg"
+              />
+              <p className="text-[#1D61E7]">My Account</p>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div> */}
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 gap-[64px]">
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
             <li>
-              <a className="">Item 1</a>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
             </li>
             <li>
-              <a>Item 1</a>
+              <a>Settings</a>
             </li>
             <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Item 1</a>
+              <a>Logout</a>
             </li>
           </ul>
+          {/* </div> */}
         </div>
-        {/* <div className="navbar-end">
-          <a className="btn">Button</a>
-        </div> */}
-      {/* </div> */}
+      </div>
+
+      <div className="bg-white navbar-center hidden lg:flex justify-center border border-t-[1px] border-b-[1px] border-l-0 border-r-0 border-gray-300 w-full h-[72px]">
+        <ul className="menu menu-horizontal px-1 gap-[64px]">
+          {/* Home */}
+          <li>
+            <input
+              type="radio"
+              name="nav-menu"
+              id="nav-home"
+              className="hidden peer"
+              defaultChecked
+            />
+            <label
+              htmlFor="nav-home"
+              className="peer-checked:bg-primary peer-checked:text-black rounded-[10px] p-[15px] gap-[5px] text-[16px] font-normal flex items-center cursor-pointer transition">
+              <img src="/src/assets/Home.svg" alt="home" />
+              Home
+            </label>
+          </li>
+
+          {/* Employees */}
+          <li>
+            <input
+              type="radio"
+              name="nav-menu"
+              id="nav-employees"
+              className="hidden peer"
+            />
+            <label
+              htmlFor="nav-employees"
+              className="peer-checked:bg-primary peer-checked:text-black rounded-[10px] p-[15px] gap-[5px] text-[16px] font-normal flex items-center cursor-pointer transition">
+              <img src="/src/assets/Employees.svg" alt="employees" />
+              Employees
+            </label>
+          </li>
+
+          {/* Records */}
+          <li>
+            <input
+              type="radio"
+              name="nav-menu"
+              id="nav-records"
+              className="hidden peer"
+            />
+            <label
+              htmlFor="nav-records"
+              className="peer-checked:bg-primary peer-checked:text-black rounded-[10px] p-[15px] gap-[5px] text-[16px] font-normal flex items-center cursor-pointer transition">
+              <img src="/src/assets/Record.svg" alt="records" />
+              Records
+            </label>
+          </li>
+
+          {/* Reports */}
+          <li>
+            <input
+              type="radio"
+              name="nav-menu"
+              id="nav-reports"
+              className="hidden peer"
+            />
+            <label
+              htmlFor="nav-reports"
+              className="peer-checked:bg-primary peer-checked:text-black rounded-[10px] p-[15px] gap-[5px] text-[16px] font-normal flex items-center cursor-pointer transition">
+              <img src="/src/assets/Report.svg" alt="reports" />
+              Reports
+            </label>
+          </li>
+
+          {/* QR */}
+          <li>
+            <input
+              type="radio"
+              name="nav-menu"
+              id="nav-qr"
+              className="hidden peer"
+            />
+            <label
+              htmlFor="nav-qr"
+              className="peer-checked:bg-primary peer-checked:text-black rounded-[10px] p-[15px] gap-[5px] text-[16px] font-normal flex items-center cursor-pointer transition">
+              <img src="/src/assets/QR.svg" alt="qr" />
+              QR
+            </label>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
